0f8a20e3be054aecd5352639b1d8ea2956f03269
soyunmate <gm.barahona911@gmail.com>
2022-11-30 23:07:01 -0300

    add final js and refactor

--- script.js
+++ script.js
@@ -2,58 +2,86 @@
 
 const btnArray = [...document.querySelectorAll(".btn")];
 const score = document.querySelector(".score");
+const message = document.querySelector(".round-message");
+const computerPreview = document.querySelector(".computer-preview");
+const finalmessage = document.querySelector(".game-state");
+const btnRestart = document.querySelector(".btn-restart");
+let currentScore = 0;
+let userChoice;
+let computerChoice;
 
 const getComputerChoice = function () {
   const x = Math.floor(Math.random() * 3) + 1;
-  if (x === 1) return "Rock";
-  if (x === 2) return "Paper";
-  if (x === 3) return "Scissors";
+  if (x === 1) return "rock";
+  if (x === 2) return "paper";
+  if (x === 3) return "scissors";
 };
 
-let computerChoice;
-let userChoice;
+const updateScore = function () {
+  currentScore += 1;
+  score.textContent = currentScore;
+};
+
+const updateMessage = function (userChoice, computerChoice, result) {
+  if (result === "win")
+    message.textContent = `${userChoice} beats ${computerChoice}. You ${result}!`;
+
+  if (result === "lose")
+    message.textContent = `${computerChoice} beats ${userChoice}. You ${result}!`;
+
+  if (result === "draw")
+    message.textContent = `${userChoice} vs ${computerChoice}... Its a ${result}!`;
+};
 
 const startGame = function (e) {
   const userChoice = e.target.dataset.answer;
-  const computerChoice = getComputerChoice().toLowerCase();
+  const computerChoice = getComputerChoice();
+  computerPreview.src = `./img/${computerChoice}.png`;
 
-  const validAnswer = ["rock", "paper", "scissors"];
-  let result;
-  if (!validAnswer.includes(userChoice)) {
-    alert("Not a valid choice!");
-    return;
+  if (computerChoice === userChoice) {
+    updateMessage(userChoice, computerChoice, "draw");
   }
-
-  if (computerChoice === userChoice)
-    result = `${userChoice} vs ${computerChoice}... Its a draw!`;
-
   // user pick rock
   if (userChoice === "rock" && computerChoice === "paper") {
-    result = "Paper beat rock. You Lose!";
+    updateMessage(userChoice, computerChoice, "lose");
   } else if (userChoice === "rock" && computerChoice === "scissors") {
-    result = "Rock beat scissors. You Win!";
+    updateMessage(userChoice, computerChoice, "win");
+    updateScore();
   }
 
   // user pick paper
   if (userChoice === "paper" && computerChoice === "scissors") {
-    result = "scissors beat paper. You Lose!";
+    updateMessage(userChoice, computerChoice, "lose");
   } else if (userChoice === "paper" && computerChoice === "rock") {
-    result = "paper beat rock. You Win!";
+    updateMessage(userChoice, computerChoice, "win");
+    updateScore();
   }
 
   // User pick Scissors
   if (userChoice === "scissors" && computerChoice === "rock") {
-    result = "rock beat scissors. You Lose!";
+    updateMessage(userChoice, computerChoice, "lose");
   } else if (userChoice === "scissors" && computerChoice === "paper") {
-    result = "scissors beat paper. You Win!";
+    updateMessage(userChoice, computerChoice, "win");
+    updateScore();
   }
-  return result;
+
+  // User win the game
+  if (currentScore === 5)
+    finalmessage.textContent = "You have won against this shitty PC";
+  //else finalmessage.textContent = "Keep playing!";
 };
 
-// for (let i = 0; i < 5; i++) {
-//   console.log(startGame());
-// }
-console.log(btnArray);
+const init = function () {
+  score.textContent = 0;
+  message.textContent = "";
+  computerPreview.src = "";
+  finalmessage.textContent = "Keep playing!";
+};
+
 btnArray.forEach((btn) => {
   btn.addEventListener("click", startGame);
 });
+
+btnRestart.addEventListener("click", init);
+
+init();
